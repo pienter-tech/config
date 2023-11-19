bcacaff773ed54f1476a8eb00984d9a8a6837360
Korneel Eeckhout <korneel@pienter.tech>
2023-11-19 10:28:48 +0100

    clean iterm

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -23,9 +23,9 @@ return {
         )
         keymap.set("n", "<leader>hv", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>",
             { desc = "Toggle harpoon menu" })
-        keymap.set("n", "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)", {desc = "Go to harpoon file 1"})
-        keymap.set("n", "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)", {desc = "Go to harpoon file 2"})
-        keymap.set("n", "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)", {desc = "Go to harpoon file 3"})
-        keymap.set("n", "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)", {desc = "Go to harpoon file 4"})
+        keymap.set("n", "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>", {desc = "Go to harpoon file 1"})
+        keymap.set("n", "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>", {desc = "Go to harpoon file 2"})
+        keymap.set("n", "<leader>h3", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>", {desc = "Go to harpoon file 3"})
+        keymap.set("n", "<leader>h4", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>", {desc = "Go to harpoon file 4"})
     end,
 }
