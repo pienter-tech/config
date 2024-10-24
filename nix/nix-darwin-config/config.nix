206fb21ead48a138a26ab53601d81977451f6cdf
Korneel Eeckhout <korneel@pienter.tech>
2024-10-24 13:19:46 +0200

    install packages

--- nix/nix-darwin-config/config.nix
+++ nix/nix-darwin-config/config.nix
@@ -1,30 +1,32 @@
-{ pkgs, ... }: {
-    # List packages installed in system profile. To search by name, run:
-    # $ nix-env -qaP | grep wget
-    environment.systemPackages =
-    [ pkgs.vim
-      pkgs.raycast
-      pkgs.karabiner-elements
-      pkgs.alejandra
-    ];
+{pkgs, ...}: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [
+    pkgs.vim
+    pkgs.raycast
+    pkgs.karabiner-elements
+    pkgs.alejandra
+    pkgs.bat
+    pkgs.fd
+  ];
 
-    # Auto upgrade nix package and the daemon service.
-    services.nix-daemon.enable = true;
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
 
-    # Necessary for using flakes on this system.
-    nix.settings.experimental-features = "nix-command flakes";
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
 
-    # Create /etc/zshrc that loads the nix-darwin environment.
-    programs.zsh.enable = true;  # default shell on catalina
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true; # default shell on catalina
 
-    # Used for backwards compatibility, please read the changelog before changing.
-    # $ darwin-rebuild changelog
-    system.stateVersion = 5;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
 
-    # The platform the configuration will be used on.
-    nixpkgs.hostPlatform = "aarch64-darwin";
-    nixpkgs.config.allowUnfree = true;
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  nixpkgs.config.allowUnfree = true;
 
-    # enable touch id in terminal
-    security.pam.enableSudoTouchIdAuth = true;
+  # enable touch id in terminal
+  security.pam.enableSudoTouchIdAuth = true;
 }
