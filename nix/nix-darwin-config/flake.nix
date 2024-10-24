80dae52f59a5e6bdc2bc780e307baf2be91dff36
Korneel Eeckhout <korneel@pienter.tech>
2024-10-24 12:37:38 +0200

    init nix

---
+++ nix/nix-darwin-config/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Korneel's MacOs Setup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."korneel" = nix-darwin.lib.darwinSystem {
+      modules = [ ./config.nix ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    # darwinPackages = self.darwinConfigurations."simple".pkgs;
+  };
+}
