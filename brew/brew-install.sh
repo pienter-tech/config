64c14f9ad0905a3cbb31e219c0ba871ea0adb104
Korneel Eeckhout <korneel@pienter.tech>
2024-11-29 14:02:10 +0100

    Add brewfiles

---
+++ brew/brew-install.sh
@@ -0,0 +1,8 @@
+# check if it was installed before now or not 
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+   echo "Brew installed" 
+fi
