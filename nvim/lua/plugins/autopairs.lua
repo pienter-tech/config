bd628cf2797381ba40dcecf41d41fb2669b871fc
Korneel Eeckhout <korneel@pienter.tech>
2024-12-10 15:27:56 +0100

    Add {%%} autopairs to autopairs.lua

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -9,12 +9,22 @@ return {
         init = function()
             local Rule = require("nvim-autopairs.rule")
             local npairs = require("nvim-autopairs")
+            local cond = require 'nvim-autopairs.conds'
 
-            npairs.add_rule(
-                Rule("*", "*", "markdown"):with_move(function(opts)
-                    return string.find(opts.text, "***", 1, true) == nil
+            npairs.add_rule(Rule("*", "*", "markdown"):with_move(function(opts)
+                return string.find(opts.text, "***", 1, true) == nil
+            end))
+
+            npairs.add_rule(Rule("%", "%", { "twig", "tmp" })
+                :with_pair(function(opts)
+                    return "{" .. "}" == opts.line:sub(opts.col - #"{", opts.col + #"}" - 1)
                 end)
-            )
+                :with_move(cond.none())
+                :with_cr(cond.none())
+                :with_del(function(opts)
+                    local col = vim.api.nvim_win_get_cursor(0)[2]
+                    return "{" .. "%" .. "%" .. "}" == opts.line:sub(col - #"{" - #"%" + 1, col + #"%" + #"}")
+                end))
         end,
     },
     {
