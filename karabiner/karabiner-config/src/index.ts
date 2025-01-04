187a6ab68b27f34b0fba2c341714f0ba5c786609
Korneel Eeckhout <korneel@pienter.tech>
2025-01-04 10:34:36 +0100

    Cleanup karabiner

--- karabiner/karabiner-config/src/index.ts
+++ karabiner/karabiner-config/src/index.ts
@@ -12,36 +12,6 @@ writeToProfile("Default profile", [
       map("s").toApp("Safari"),
       map("o").toApp("Microsoft Outlook"),
     ]),
-  modifierLayer("option", "w", "window-layer")
-    .description("Window management")
-    .leaderMode({
-      sticky: true,
-      escape: ["spacebar", "return_or_enter", "escape", "caps_lock"],
-    })
-    .notification()
-    .manipulators([
-      map("h").to$(
-        "open -g raycast://extensions/raycast/window-management/left-half",
-      ),
-      map("l").to$(
-        "open -g raycast://extensions/raycast/window-management/right-half",
-      ),
-      map("k").to$(
-        "open -g raycast://extensions/raycast/window-management/top-half",
-      ),
-      map("j").to$(
-        "open -g raycast://extensions/raycast/window-management/bottom-half",
-      ),
-      map("o").to$(
-        "open -g raycast://extensions/raycast/window-management/almost-maximize",
-      ),
-      map("p").to$(
-        "open -g raycast://customWindowManagementCommand?&name=Focus&position=center&relativeWidth=0.95&relativeHeight=0.95",
-      ),
-      map("f").to$(
-        "open -g raycast://extensions/raycast/window-management/maximize",
-      ),
-    ]),
   modifierLayer("option", "m", "music-layer")
     .description("Apple Music")
     .leaderMode()
