ddeac3c42e7ddff17e91aff0dff1f1c9121797f8
Korneel Eeckhout <korneel@pienter.tech>
2025-01-05 15:54:31 +0100

    Add raycast command: Project Notes

---
+++ raycast/commands/project-notes.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title projects/notes
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌗
+# @raycast.packageName Projects: NOTES
+
+# Documentation:
+# @raycast.author pienter-tech
+# @raycast.authorURL https://github.com/pienter-tech
+
+wezterm start --workspace NOTES --cwd "/Users/korneel/Library/Mobile Documents/iCloud~md~obsidian/Documents/two" nvim
