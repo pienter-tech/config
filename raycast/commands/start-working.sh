9079e14dedc632bfe91e95d16a9f2b3e86e9cacc
Korneel Eeckhout <korneel@pienter.tech>
2025-01-06 09:26:04 +0100

    Add startup raycast command

---
+++ raycast/commands/start-working.sh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title scripts/startup
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🚀
+# @raycast.packageName Startup
+
+# Documentation:
+# @raycast.description Opens common workspace applications (Outlook, Mail, WezTerm, Chrome, Firefox)
+# @raycast.author pienter-tech
+# @raycast.authorURL https://github.com/pienter-tech
+
+# Open Outlook
+open -a "Microsoft Outlook"
+
+# Open Apple Mail
+open -a "Mail"
+
+# Open WezTerm
+open -a "WezTerm"
+
+# Open Chrome
+open -a "Google Chrome"
+
+# Open Firefox
+open -a "Firefox"
+
+open -a "Slack"
+
+open -g raycast://extensions/raycast/raycast-ai/ai-chat
+
+open -g raycast://script-commands/project-notes
+
+echo "All applications have been launched!"
