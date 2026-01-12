5d2bf473ee431c245a3ddd21c3e465087fe49403
Korneel Eeckhout <korneel@pienter.tech>
2026-01-12 10:24:15 +0100

    feat(raycast): add mail forwarding commands for Billit

---
+++ raycast/commands/email-files-to-billit.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/osascript
+
+# @raycast.schemaVersion 1
+# @raycast.title Files to Billit
+# @raycast.mode silent
+# @raycast.packageName Billit Tools
+
+-- Get email from environment variable
+set targetEmail to do shell script "echo $BILLIT_EMAIL"
+if targetEmail is "" then
+	return "Error: BILLIT_EMAIL environment variable not set."
+end if
+
+tell application "Finder"
+	set selectedFiles to selection
+
+	if selectedFiles is {} then
+		return "Error: No files selected in Finder."
+	end if
+
+	-- Convert to POSIX paths for Mail attachment
+	set filePaths to {}
+	repeat with aFile in selectedFiles
+		set end of filePaths to (aFile as alias)
+	end repeat
+end tell
+
+tell application "Mail"
+	set newMessage to make new outgoing message with properties {subject:"Files", visible:false}
+
+	tell newMessage
+		make new to recipient at end of to recipients with properties {address:targetEmail}
+
+		-- Attach each selected file
+		repeat with aFile in filePaths
+			make new attachment with properties {file name:aFile} at after the last paragraph
+		end repeat
+
+		send
+	end tell
+end tell
+
+return "Emailed " & (count of filePaths) & " file(s) to " & targetEmail
---
+++ raycast/commands/forward-to-billit.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/osascript
+
+# @raycast.schemaVersion 1
+# @raycast.title Forward To Billit
+# @raycast.mode silent
+# @raycast.packageName Billit Tools
+
+-- Get email from environment variable
+set targetEmail to do shell script "echo $BILLIT_EMAIL"
+if targetEmail is "" then
+	return "Error: BILLIT_EMAIL environment variable not set."
+end if
+
+tell application "Mail"
+	-- Get the list of emails currently selected in the user interface
+	set selectedMessages to selection
+
+	-- Check if anything is actually selected
+	if selectedMessages is {} then
+		return "Error: No email selected in Apple Mail."
+	end if
+
+	set forwardCount to 0
+
+	-- Loop through each selected message (in case you selected multiple)
+	repeat with aMessage in selectedMessages
+		try
+			-- Create the forwarded message
+			set forwardMessage to forward aMessage
+
+			-- Configure the forwarded message
+			tell forwardMessage
+				make new to recipient at end of to recipients with properties {address:targetEmail}
+				-- remove the next line if you want to review the email before sending
+				send
+			end tell
+
+			set forwardCount to forwardCount + 1
+		on error
+			-- If one fails, continue to the next
+		end try
+	end repeat
+
+	return "Forwarded " & forwardCount & " email(s) to " & targetEmail
+end tell
+
